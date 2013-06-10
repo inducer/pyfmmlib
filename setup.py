e9b00f8ef58857e17ed9bc326a7cf56286142564
Andreas Kloeckner <inform@tiker.net>
2013-06-10 12:09:02 -0400

    Get rid of ez_setup

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-import ez_setup
-ez_setup.use_setuptools()
-
 GENERATED_SOURCES = ["wrappers.pyf", "vec_wrappers.f90"]
+
 
 # {{{ wrapper generation
 
@@ -13,11 +11,13 @@ def cpre(s):
     else:
         return s
 
+
 def cpost(s):
     if s:
         return s + ", "
     else:
         return s
+
 
 def generate_wrappers():
     from vec_wrappers import gen_vector_wrappers
@@ -26,17 +26,18 @@ def generate_wrappers():
 
     base_name = "wrappers.pyf"
     mako_name = base_name + ".mako"
-    tmpl = Template(open(mako_name, "rt").read(), uri=mako_name, strict_undefined=True)
+    tmpl = Template(open(mako_name, "rt").read(),
+            uri=mako_name, strict_undefined=True)
 
     context = dict(cpre=cpre, cpost=cpost,
-            gen_vector_wrappers=gen_vector_wrappers
-            )
+            gen_vector_wrappers=gen_vector_wrappers)
     result = tmpl.render(**context)
     open("wrappers.pyf", "wt").write(result)
 
     open("vec_wrappers.f90", "wt").write(gen_vector_wrappers())
 
 # }}}
+
 
 def count_down_delay(delay):
     from time import sleep
@@ -48,26 +49,31 @@ def count_down_delay(delay):
         sleep(1)
     print("")
 
+
+DASH_SEPARATOR = 75 * "-"
+
+
 def main():
     try:
-        import mako
+        import mako  # noqa
     except ImportError:
-        print("----------------------------------------------------------------------------")
+        print(DASH_SEPARATOR)
         print("Mako is not installed.")
-        print("----------------------------------------------------------------------------")
+        print(DASH_SEPARATOR)
 
         from os.path import exists
         if all(exists(s) for s in GENERATED_SOURCES):
             print("pyfmmlib uses mako [1] to generate its wrappers.")
             print("All the generated files are there, so we'll continue.")
-            print("----------------------------------------------------------------------------")
+            print(DASH_SEPARATOR)
             print("Hit Ctrl-C now if you'd like to think about the situation.")
-            print("----------------------------------------------------------------------------")
+            print(DASH_SEPARATOR)
 
             count_down_delay(delay=5)
 
         else:
-            print("That is a problem because pyfmmlib uses mako [1] to generate its wrappers.")
+            print("That is a problem because pyfmmlib uses mako [1] "
+                    "to generate its wrappers.")
             print("Try typing")
             print("")
             print("  pip install mako")
@@ -75,31 +81,32 @@ def main():
             print("or")
             print("  ez_install mako")
             print("")
-            print("That should install it. If that doesn't work, go to the mako website,")
+            print("That should install it. If that doesn't work, "
+                    "go to the mako website,")
             print("download, and install by calling 'python setup.py install'.")
             print("")
             print("[1] http://www.makotemplates.org/")
-            print("----------------------------------------------------------------------------")
+            print(DASH_SEPARATOR)
             import sys
             sys.exit(1)
     else:
         generate_wrappers()
 
     # looks pointless, but allows 'python setup.py develop'--do not remove
-    import setuptools
+    import setuptools  # noqa
 
     from numpy.distutils.core import Extension, setup
 
     from os.path import exists
     if not exists("fmmlib2d") or not exists("fmmlib3d"):
-        print("----------------------------------------------------------------")
+        print(DASH_SEPARATOR)
         print("Missing fmmlib sources")
-        print("----------------------------------------------------------------")
+        print(DASH_SEPARATOR)
         print("These can be downloaded by running ./grab-sources.sh.")
         print("Note that this will fail on Windows--it's a shell script.")
-        print("----------------------------------------------------------------")
+        print(DASH_SEPARATOR)
         print("I will try to do that after a short delay, unless you stop me.")
-        print("----------------------------------------------------------------")
+        print(DASH_SEPARATOR)
         count_down_delay(delay=10)
 
         from subprocess import check_call
@@ -133,36 +140,34 @@ def main():
           long_description=open("README.rst", "rt").read(),
           author="Leslie Greengard, Zydrunas Gimbutas, Andreas Kloeckner",
           author_email="inform@tiker.net",
-          license = "wrapper: MIT/code: GPL2",
+          license="wrapper: MIT/code: GPL2",
           url="http://github.com/inducer/pyfmmlib",
           classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Other Audience',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-            'Programming Language :: Fortran',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Software Development :: Libraries',
-            ],
+              'Development Status :: 4 - Beta',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Other Audience',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+              'Programming Language :: Fortran',
+              'Programming Language :: Python',
+              'Topic :: Scientific/Engineering',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'Topic :: Software Development :: Libraries',
+              ],
 
-          packages = [ "pyfmmlib" ],
+          packages=["pyfmmlib"],
           install_requires=[
               "pytest>=2",
               ],
-          ext_modules = [
-            Extension(
-              "pyfmmlib._internal",
-              source_files,
-              extra_link_args=extra_link_args,
-              ),
-            ]
-         )
-
-
+          ext_modules=[
+              Extension(
+                  "pyfmmlib._internal",
+                  source_files,
+                  extra_link_args=extra_link_args,
+                  ),
+              ]
+          )
 
 
 if __name__ == '__main__':
