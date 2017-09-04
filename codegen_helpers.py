36fc20c476c8f9ea71e77dd0681942f45f8c83cb
Andreas Kloeckner <inform@tiker.net>
2017-09-04 13:23:26 -0500

    Remove dead code, add cpre, cpost

--- codegen_helpers.py
+++ codegen_helpers.py
@@ -35,12 +35,6 @@ class Laplace(Equation):
         else:
             raise RuntimeError("invalid dimensionality")
 
-    def local_eval_cache_dims(self, nterms="nterms"):
-        if self.dimensions == 2:
-            return "0:%(nterms)s" % locals()
-        else:
-            raise NotImplementedError("unimplemented dimensionality")
-
     def lh_letter(self):
         return "l"
 
@@ -70,12 +64,6 @@ class Helmholtz(Equation):
         else:
             raise RuntimeError("invalid dimensionality")
 
-    def local_eval_cache_dims(self, nterms="nterms"):
-        if self.dimensions == 2:
-            return "-(%(nterms)s)-2:(%(nterms)s)+2" % locals()
-        else:
-            raise NotImplementedError("unimplemented dimensionality")
-
     def lh_letter(self):
         return "h"
 
@@ -85,60 +73,15 @@ class Helmholtz(Equation):
 # }}}
 
 
-class SumComputation:
-    def __init__(self, eqn, outputs, source_derivative=False):
-        """
-        :param source_derivative: a :class:`bool`.
-        """
+def cpre(s):
+    if s:
+        return ", "+s
+    else:
+        return s
 
-        self.eqn = eqn
-        self.outputs = outputs
-        self.source_derivative = source_derivative
 
-        if source_derivative and isinstance(eqn, Laplace) and eqn.dimensions == 2:
-            raise NotImplementedError("2D double-layer Laplace")
-
-    def eqn_and_dim(self):
-        return "%s%dd" % (
-                self.eqn.lh_letter(),
-                self.dimensions)
-
-    def what(self):
-        if self.source_derivative:
-            src_deriv = "_sd"
-        else:
-            src_deriv = ""
-
-        return "%s%s%s_%dd" % (
-                self.eqn.lh_letter(),
-                self.outputs, src_deriv,
-                self.dimensions)
-
-    @property
-    def dimensions(self):
-        return self.eqn.dimensions
-
-    def output_arg_names_and_dims(self, force_pot=False):
-        saw_pot = False
-
-        for o in self.outputs:
-            if o == "p":
-                saw_pot = True
-                yield "pot", ()
-            elif o == "f":
-                yield "fld", (self.dimensions,)
-            elif o == "g":
-                yield "grad", (self.dimensions,)
-            elif o == "h":
-                yield "hess", (hess_size(self.dimensions),)
-            else:
-                raise ValueError("unknown output name")
-
-        if force_pot and not saw_pot:
-            yield "pot", ()
-
-    def get_output_kwargs(self, suffix="", force_pot=False):
-        return dict(
-                (name, name+suffix)
-                for name, dims in
-                self.output_arg_names_and_dims(force_pot=force_pot))
+def cpost(s):
+    if s:
+        return s + ", "
+    else:
+        return s
