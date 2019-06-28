eb8ed8516e6adf70fd900fad09de301f05e2ab3b
Matt Wala <wala1@illinois.edu>
2019-06-28 14:02:40 -0500

    Change reduction variables to firstprivate

--- vec_wrappers.py
+++ vec_wrappers.py
@@ -328,11 +328,13 @@ def get_vector_wrapper(func_name, args, out_args, vec_func_name=None,
 
     if has_indirect_many:
         private_vars = ["icsr", "ncsr_count"]
+        firstprivate_vars = []
         for type_, name, shape in args:
             if shape and name in out_args and MANY_MARKER not in shape:
-                private_vars.append(name + "_tmp")
+                firstprivate_vars.append(name + "_tmp")
 
         extra_omp += " private(%s)" % ", ".join(private_vars)
+        extra_omp += " firstprivate(%s)" % ", ".join(firstprivate_vars)
 
     shared_vars = ["nvcount"]
     for type_, name, shape in args:
@@ -625,7 +627,8 @@ def gen_vector_wrappers():
                         !$omp parallel do default(none) schedule(dynamic, 10) &
                         !$omp private(tgt_icenter, center, rscale, itgt_box, &
                         !$omp   isrc_box_start, isrc_box_stop, src_ibox, &
-                        !$omp   isrc_start, expn_tmp, ier_tmp) &
+                        !$omp   isrc_start) &
+                        !$omp firstprivate(expn_tmp, ier_tmp) &
                         !$omp shared(ier,  ${eqn.in_arg_list()|cpost} &
                         !$omp   nsources, sources, &
                                     %if dp_or_no:
