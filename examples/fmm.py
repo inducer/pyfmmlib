06428d04d7f0a98fc7b43905bf57edfb31bfeaf7
Matt Wala <wala1@illinois.edu>
2020-07-22 21:16:22 -0500

    Add an examples job and make examples/fmm.py runnable as an example

--- examples/fmm.py
+++ examples/fmm.py
@@ -1,19 +1,29 @@
-from __future__ import division
+#!/usr/bin/env python3
+"""Computes a Helmholtz potential with the FMM.
+"""
 
 from pyfmmlib import fmm_part, HelmholtzKernel
 import numpy as np
 import matplotlib.pyplot as pt
 
-sources = np.random.randn(40, 2)
 
-targets = np.mgrid[-7:7:400j, -7:7:400j]
-pot_shape = targets.shape[1:]
-targets = targets.reshape(2, -1)
+def main():
+    sources = np.random.randn(40, 2)
 
-pot, grad = fmm_part("PG", iprec=2, kernel=HelmholtzKernel(5),
-        sources=sources, mop_charge=1, target=targets.T)
+    targets = np.mgrid[-7:7:400j, -7:7:400j]
+    pot_shape = targets.shape[1:]
+    targets = targets.reshape(2, -1)
 
-pot = pot.reshape(pot_shape)
+    pot, grad = fmm_part("PG", iprec=2, kernel=HelmholtzKernel(5),
+            sources=sources, mop_charge=1, target=targets.T)
 
-pt.imshow(pot.real)
-pt.show()
+    pot = pot.reshape(pot_shape)
+
+    pt.imshow(pot.real)
+    outfile = "helmholtz-potential.png"
+    pt.savefig(outfile)
+    print("wrote '%s'" % outfile)
+
+
+if __name__ == "__main__":
+    main()
