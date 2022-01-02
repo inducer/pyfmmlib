eeba60205ca5aee61915fe7deff4a1fed48800fb
Andreas Kloeckner <inform@tiker.net>
2022-01-02 14:46:42 +0100

    Improve setup.py code quality

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ GENERATED_SOURCES = ["wrappers.pyf", "vec_wrappers.f90"]
 
 # {{{ wrapper generation
 
-
 def generate_wrappers():
     from vec_wrappers import gen_vector_wrappers
     from mako.template import Template
@@ -13,14 +12,14 @@ def generate_wrappers():
 
     base_name = "wrappers.pyf"
     mako_name = base_name + ".mako"
-    tmpl = Template(open(mako_name, "rt").read(), uri=mako_name, strict_undefined=True)
+    with open(mako_name, "rt") as inf:
+        tmpl = Template(inf.read(), uri=mako_name, strict_undefined=True)
 
     context = dict(cpre=cpre, cpost=cpost, gen_vector_wrappers=gen_vector_wrappers)
     result = tmpl.render(**context)
     open("wrappers.pyf", "wt").write(result)
 
     open("vec_wrappers.f90", "wt").write(gen_vector_wrappers())
-
 
 # }}}
 
@@ -50,34 +49,34 @@ def main():
     build_mode = os.environ.get("PYFMMLIB_BUILD_MODE", "openmp-opt")
 
     if build_mode == "openmp-ofast":
-        FOPT_ARGS = "-Ofast -fopenmp"
-        OPT_ARGS = "-Ofast"
-        EXTRA_LINK_ARGS = "-fopenmp"
+        fopt_args = "-Ofast -fopenmp"
+        opt_args = "-Ofast"
+        extra_link_args = "-fopenmp"
 
     elif build_mode == "openmp-opt":
-        FOPT_ARGS = "-O3 -fopenmp"
-        OPT_ARGS = "-O3 -fopenmp"
-        EXTRA_LINK_ARGS = "-fopenmp"
+        fopt_args = "-O3 -fopenmp"
+        opt_args = "-O3 -fopenmp"
+        extra_link_args = "-fopenmp"
 
     elif build_mode == "debug":
-        FOPT_ARGS = "-g"
-        OPT_ARGS = "-g"
-        EXTRA_LINK_ARGS = "-g"
+        fopt_args = "-g"
+        opt_args = "-g"
+        extra_link_args = "-g"
 
     elif build_mode == "setuptools":
-        FOPT_ARGS = os.environ.get("FOPT", "")
-        OPT_ARGS = os.environ.get("OPT", "")
-        EXTRA_LINK_ARGS = os.environ.get("EXTRA_LINK_ARGS", "")
+        fopt_args = os.environ.get("FOPT", "")
+        opt_args = os.environ.get("OPT", "")
+        extra_link_args = os.environ.get("extra_link_args", "")
 
     else:
         raise ValueError("invalid value of $PYFMMLIB_BUILD_MODE")
 
     # NOTE: gfortran 8.1+ errors on incorrect dummy argument shape
     # https://groups.google.com/forum/#!topic/comp.lang.fortran/x3JnAjRX-KA
-    os.environ["FOPT"] = "-std=legacy {}".format(FOPT_ARGS)
+    os.environ["FOPT"] = "-std=legacy {}".format(fopt_args)
 
-    os.environ["OPT"] = OPT_ARGS
-    os.environ["EXTRA_LINK_ARGS"] = EXTRA_LINK_ARGS
+    os.environ["OPT"] = opt_args
+    os.environ["EXTRA_LINK_ARGS"] = extra_link_args
 
     try:
         import mako  # noqa
@@ -131,6 +130,7 @@ def main():
 
     # looks pointless, but allows 'python setup.py develop'--do not remove
     import setuptools  # noqa
+    from setuptools import find_packages
 
     from numpy.distutils.core import Extension, setup
 
@@ -139,11 +139,11 @@ def main():
 
     source_files = {}
 
-    BLACKLIST = ["d2tstrcr_omp.f", "second-r8.f"]  # noqa
+    BLOCKLIST = ["d2tstrcr_omp.f", "second-r8.f"]  # noqa
 
     for f in glob("fmmlib2d/src/*.f") + glob("fmmlib3d/src/*.f"):
         bn = basename(f)
-        if bn in BLACKLIST:
+        if bn in BLOCKLIST:
             continue
 
         source_files[bn] = f
@@ -157,11 +157,8 @@ def main():
         extra_link_args = []
 
     ver_dic = {}
-    version_file = open("pyfmmlib/version.py")
-    try:
+    with open("pyfmmlib/version.py") as version_file:
         version_file_contents = version_file.read()
-    finally:
-        version_file.close()
 
     exec(compile(version_file_contents, "pyfmmlib/version.py", "exec"), ver_dic)
 
@@ -187,7 +184,7 @@ def main():
             "Topic :: Scientific/Engineering :: Mathematics",
             "Topic :: Software Development :: Libraries",
         ],
-        packages=["pyfmmlib"],
+        packages=find_packages(),
         python_requires="~=3.6",
         setup_requires=[
             "numpy",
