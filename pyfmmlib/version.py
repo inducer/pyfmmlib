bceccc169cff67917f71be0312c358cdd5896f82
Andreas Kloeckner <inform@tiker.net>
2024-08-12 14:15:07 +0200

    Fix ruff C409

--- pyfmmlib/version.py
+++ pyfmmlib/version.py
@@ -2,5 +2,5 @@ from importlib import metadata
 
 
 VERSION_TEXT = metadata.version("pyfmmlib")
-VERSION = tuple([int(v) for v in VERSION_TEXT.split(".")])
+VERSION = tuple(int(v) for v in VERSION_TEXT.split("."))
 VERSION_STATUS = ""
